9e06e1722b4e4debd5a9e02a713e1b5a43b7add1
Lauri Litovuo <llitovuo@student.hive.fi>
2025-01-16 14:14:21 +0200

    day01

---
+++ day1/helloworld.py
@@ -0,0 +1,30 @@
+#Day 1 - 30 Days of Python
+print(2 + 3) # addition(+)
+print(3 - 1) # subtraction(-)
+print(2 * 3) # multiplication(*)
+print(3 / 2) # division(/)
+print(3 ** 2) # exponential(**)
+print(3 % 2) # modulus(%)
+print(3 // 2) # Floor division operator(//)
+
+# Checking data types
+print(type(10)) # Int
+print(type(9.8)) # Float
+print(type(3.14)) # Float
+print(type(4 + 4j)) # Complex
+print(type('Lauri')) # String
+print(type(['Asabeneh', 'Python', 'Finland'])) # List
+print(type({'name':'Lauri', 'Last name':'Litovuo'})) # Dictionary
+print(type((1,2))) # Tuple
+print(type(({1,2,3}))) # Set
+
+print('EX02')
+
+if (2 > 3):
+	print('2 is greater than 3')
+
+
+print('EX03')
+import math
+#eucledian distance	of  (2, 3) and (10, 8
+print(math.sqrt((10-2)**2 + (8-3)**2))
